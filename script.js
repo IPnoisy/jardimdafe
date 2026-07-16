260c6ac7b246ad2a16713b128b4ed0e18898f021
IPnoisy <ipnoisyy@gmail.com>
2026-07-16 00:09:57 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+/* ==========================
+      JARDIM DA FÉ
+      script.js
+========================== */
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const faqItems = document.querySelectorAll(".faq-item");
+
+    faqItems.forEach(item => {
+
+        const button = item.querySelector(".faq-question");
+
+        button.addEventListener("click", () => {
+
+            const isActive = item.classList.contains("active");
+
+            faqItems.forEach(faq => {
+
+                faq.classList.remove("active");
+
+            });
+
+            if (!isActive) {
+
+                item.classList.add("active");
+
+            }
+
+        });
+
+    });
+
+});
